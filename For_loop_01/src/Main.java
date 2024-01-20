4d1a4c9b9b8d70ac854c2a2f99f843af0cd4e2a4
Shubham Suratkar <suratkarinbox@gmail.com>
2024-01-20 17:34:20 +0530

    This is basic program of for loop

---
+++ For_loop_01/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println((13 * counter)); // This will print the multiplication table of 13.
+            // If you want to print the table of a different number, replace 13 with that number.
+        }
+    }
+}
